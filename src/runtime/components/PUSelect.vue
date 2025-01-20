f3072f97883dcb4eb406d9469eada98b725a3f06
Felipe Oliveira <felipe16.12.1998@gmail.com>
2025-01-20 11:19:18 -0300

    feat(components): Add new component Select

--- src/runtime/components/PUSelect.vue
+++ src/runtime/components/PUSelect.vue
@@ -6,30 +6,26 @@ type Option = {
   label: string
 }
 
-// Propriedades recebidas pelo componente
 const props = defineProps<{
-  options: Option[] // Lista de opções
-  modelValue: string | number | null // Valor selecionado
-  placeholder?: string // Placeholder opcional
-  size?: 'small' | 'large' // Tamanho do select
-  exampleOption?: Option // Opção de exemplo a ser exibida inicialmente
+  options: Option[]
+  modelValue: string | number | null
+  placeholder?: string
+  size?: 'small' | 'large'
+  id?: string
+  exampleOption?: Option
 }>()
 
-// Emitir eventos para o componente pai
 const emit = defineEmits(['update:modelValue'])
 
-// Controle interno do valor selecionado
 const selectedValue = ref<string | number | null>(null)
 
 onMounted(() => {
-  // Definir o valor inicial como o exemplo, se fornecido
   if (props.exampleOption) {
     selectedValue.value = props.exampleOption.value
     emit('update:modelValue', props.exampleOption.value)
   }
 })
 
-// Atualizar o valor quando o usuário selecionar uma nova opção
 const handleChange = (event: Event) => {
   const target = event.target as HTMLSelectElement
   selectedValue.value = target.value
@@ -40,6 +36,7 @@ const handleChange = (event: Event) => {
 <template>
   <div class="relative w-full">
     <select
+      :id="id"
       class="appearance-none cursor-pointer border rounded-lg px-4 py-2 text-primary-light-500 bg-white focus:outline-none ring-2 ring-primary-light-500 w-full"
       :value="selectedValue"
       @change="handleChange"
