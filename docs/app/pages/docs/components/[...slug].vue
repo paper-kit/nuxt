39c1d828265bb03a8d059fc6cb36031d124f219d
Felipe <felipe16.12.1998@gmail.com>
2025-02-04 22:01:49 -0300

    feat(docs): add components page

---
+++ docs/app/pages/docs/components/[...slug].vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+const route = useRoute()
+
+const { data: page } = await useAsyncData('page-' + route.path, () => {
+  return queryCollection('content').path(route.path).first()
+})
+
+if (!page.value) {
+  throw createError({
+    statusCode: 404,
+    statusMessage: 'Page not found',
+    fatal: true,
+  })
+}
+</script>
+
+<template>
+  <div class="overflow-y-auto flex py-4 h-auto w-full gap-4">
+    <AsideMenu />
+    <div class="w-full flex flex-col gap-4">
+      <ContentRenderer
+        v-if="page"
+        :value="page"
+      />
+    </div>
+  </div>
+</template>
