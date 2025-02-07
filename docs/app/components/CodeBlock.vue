fe532e1358488066be70c656a810580c949b0cac
Felipe <felipe16.12.1998@gmail.com>
2025-02-06 22:25:17 -0300

    feat(docs): add CodeBlock and use on pu-accordion page

---
+++ docs/app/components/CodeBlock.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="border-2 border-primary-light-500 rounded-lg">
+    <div class="flex border-b border-primary-light-500">
+      <button
+        v-for="tab in tabs"
+        :key="tab.label"
+        class="px-4 py-2 font-medium transition-colors duration-200"
+        :class="{
+          'bg-primary-light-500 text-white': activeTab === tab.label,
+          'bg-gray-100 text-gray-700': activeTab !== tab.label,
+        }"
+        @click="activeTab = tab.label"
+      >
+        {{ tab.label }}
+      </button>
+    </div>
+    <div class="p-6 bg-gradient-to-b from-white to-gray-50 rounded-xl shadow-lg h-auto">
+      <div
+        v-if="activeTab === 'Preview'"
+        class="w-full h-full flex justify-center items-center gap-2"
+      >
+        <slot name="preview" />
+      </div>
+      <div v-else>
+        <CodeBox
+          type="vue"
+          header="app.vue"
+        >
+          <slot name="code" />
+        </CodeBox>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const tabs = [
+  { label: 'Preview' },
+  { label: 'Code' },
+]
+const activeTab = ref(tabs && tabs.length ? tabs[0].label : '')
+</script>
--- docs/content/docs/components/pu-accordion.md
+++ docs/content/docs/components/pu-accordion.md
@@ -1,1 +1,25 @@
-pu-accordion
+::paper-p-u-accordion
+#code-default
+```html
+<template>
+  <PUAccordion :items="accordionItems" />
+</template>
+
+<script setup lang="ts">
+const accordionItems = [
+  {
+    title: "Accordion 1",
+    content: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+  },
+  {
+    title: "Accordion 2",
+    content: "Incidunt a voluptatum reiciendis soluta est unde consectetur!",
+  },
+  {
+    title: "Accordion 3",
+    content: "Explicabo, odio molestias! Quaerat, ipsa fugit quasi atque.",
+  },
+];
+</script>
+```
+::
