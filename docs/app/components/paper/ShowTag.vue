2d60b29474a534fd23d8e3a173afe58f62e65bb3
Felipe <felipe16.12.1998@gmail.com>
2025-02-09 20:10:34 -0300

    feat(docs): add docs page for tag

---
+++ docs/app/components/paper/ShowTag.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="tag-demo">
+    <CodeBlock v-if="type === 'default'">
+      <template #preview>
+        <PUTag label="Default Tag" />
+      </template>
+      <template #code>
+        <slot name="code" />
+      </template>
+    </CodeBlock>
+
+    <CodeBlock v-if="type === 'with-icon'">
+      <template #preview>
+        <PUTag
+          label="Tag with Icon"
+          icon="clear-x"
+        />
+      </template>
+      <template #code>
+        <slot name="code" />
+      </template>
+    </CodeBlock>
+  </div>
+</template>
+
+<script lang="ts" setup>
+defineProps<{
+  type: 'default' | 'with-icon'
+}>()
+</script>
