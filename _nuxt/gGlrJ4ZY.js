9cf8db7d77b3f967c394799d73a55ccd3698340c
FelipeO16 <85635331+FelipeO16@users.noreply.github.com>
2025-02-10 00:23:16 +0000

    Deploying to gh-pages from @ paper-kit/nuxt@d196d6348beb8de0f68a26598ee00367c357cb31 🚀

--- _nuxt/gGlrJ4ZY.js
+++
@@ -1,1 +0,0 @@
-import{a,f as o,k as s,i as t,Y as _,l as n}from"./CWI_tegl.js";const r={class:"pu-label font-patrick"},c=["for"],l=a({__name:"PULabel",props:{id:{}},setup(p){return(e,d)=>(s(),o("div",r,[t("label",{for:e.id},[_(e.$slots,"default",{},void 0,!0)],8,c)]))}}),f=Object.assign(n(l,[["__scopeId","data-v-c90a41a1"]]),{__name:"PULabel"});export{f as _};
