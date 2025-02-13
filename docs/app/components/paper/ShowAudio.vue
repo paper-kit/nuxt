c230090f98bd973525ea96c507e95706c064d28c
Felipe <felipe16.12.1998@gmail.com>
2025-02-13 12:48:42 -0300

    feat(docs): add docs page for audio

---
+++ docs/app/components/paper/ShowAudio.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="audio-demo">
+    <CodeBlock>
+      <template #preview>
+        <PUAudio src="/paper-ui.mp3" />
+      </template>
+      <template #code>
+        <slot name="code" />
+      </template>
+    </CodeBlock>
+  </div>
+</template>
+
+<script lang="ts" setup>
+defineProps<{
+  type?: 'default'
+}>()
+</script>
