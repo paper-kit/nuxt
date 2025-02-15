9d07ea93eae44edb3754475f06b28d83c410f402
Felipe <felipe16.12.1998@gmail.com>
2025-02-15 10:13:24 -0300

    feat: initial tests with darkmode

--- docs/app/components/ShowComponent.vue
+++ docs/app/components/ShowComponent.vue
@@ -1,15 +1,24 @@
 <template>
-  <div class="border-2 border-primary-light-500 rounded-lg w-72 h-48 hover:scale-110 transition-all duration-300 relative">
-    <div class="absolute top-2 right-2">
+  <div
+    :class="{
+      dark: isDarkMode,
+    }"
+    class="border-2 border-primary-light-500 rounded-lg w-72 h-48 hover:scale-110 transition-all duration-300 relative"
+  >
+    <div class="absolute top-2 right-2 cursor-pointer dark:text-primary-light-50">
       <PUIcon
-        v-if="isNew"
-        name="star"
+        name="brightness"
+        @click="toogleDarkMode"
       />
     </div>
-    <div class="component-example p-4 flex justify-center items-center h-36 ">
+    <div class="component-example p-4 flex justify-center items-center h-36 dark:bg-primary-light-500">
       <slot name="component" />
     </div>
-    <div class="component-name text-lg font-bold flex h-12 justify-center items-center w-full border-t-2 border-primary-light-500">
+    <div class="component-name text-lg font-bold flex h-12 gap-4 justify-center items-center w-full border-t-2 border-primary-light-500">
+      <PUIcon
+        v-if="isNew"
+        name="star-solid"
+      />
       <slot name="component-name" />
     </div>
   </div>
@@ -19,4 +28,10 @@
 defineProps<{
   isNew?: boolean
 }>()
+
+const isDarkMode = ref(false)
+
+const toogleDarkMode = () => {
+  isDarkMode.value = !isDarkMode.value
+}
 </script>
