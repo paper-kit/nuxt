d4c6961b250c43881e88a712126fcf3bfefd697e
Felipe <felipe16.12.1998@gmail.com>
2025-02-23 22:03:34 -0300

    feat(docs): add compare on codeblock

--- docs/app/components/ShowComponent.vue
+++ docs/app/components/ShowComponent.vue
@@ -1,24 +1,17 @@
 <template>
   <div
-    :class="{
-      dark: isDarkMode,
-    }"
     class="border-2 border-primary-light-500 rounded-lg w-72 h-48 hover:scale-110 transition-all duration-300 relative"
   >
     <div class="absolute top-2 right-2 cursor-pointer dark:text-primary-light-50">
       <PUIcon
-        name="brightness"
-        @click="toogleDarkMode"
+        v-if="isNew"
+        name="star-solid"
       />
     </div>
     <div class="component-example p-4 flex justify-center items-center h-36 dark:bg-primary-light-500">
       <slot name="component" />
     </div>
     <div class="component-name text-lg font-bold flex h-12 gap-4 justify-center items-center w-full border-t-2 border-primary-light-500">
-      <PUIcon
-        v-if="isNew"
-        name="star-solid"
-      />
       <slot name="component-name" />
     </div>
   </div>
@@ -28,10 +21,4 @@
 defineProps<{
   isNew?: boolean
 }>()
-
-const isDarkMode = ref(false)
-
-const toogleDarkMode = () => {
-  isDarkMode.value = !isDarkMode.value
-}
 </script>
